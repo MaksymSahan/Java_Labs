c98ef311a30a2c987e10ee1240b50b2f6e9c72a2
MaksymSahan <max.sagan.2000@gmail.com>
2018-06-11 15:21:54 +0300

    fixed lab1

--- Lab_1_Java/Лаба_1/Main.java
+++ Lab_1_Java/Лаба_1/Main.java
@@ -4,21 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
         {
-            Printer epson_L366 = new Printer();
-            Printer hp_LaserJet_Pro_M130a = new Printer("HP_LaserJet_Pro_M130a", 6.8, "White", "LaserPrint");
-            Printer ricoh_MP_2014AD = new Printer("Ricoh_MP_2014AD", 29, "Black", 500, "LaserPrint");
+            Printer epsonL366 = new Printer();
+            Printer hpLaserJetProM130a = new Printer("HP_LaserJet_Pro_M130a", 6.8, "White", "LaserPrint");
+            Printer ricohMP2014AD = new Printer("Ricoh_MP_2014AD", 29, "Black", 500, "LaserPrint");
 
-            System.out.println(epson_L366.toString());
-            System.out.println(hp_LaserJet_Pro_M130a.toString());
-            System.out.println(ricoh_MP_2014AD.toString());
+            System.out.println(epsonL366.toString());
+            System.out.println(hpLaserJetProM130a.toString());
+            System.out.println(ricohMP2014AD.toString());
 
-            epson_L366.resetValues("Epson_L366", 4.5, "Black", 70, "JetPrint");
+            epsonL366.resetValues("Epson_L366", 4.5, "Black", 70, "JetPrint");
 
-            epson_L366.printSum();
-            hp_LaserJet_Pro_M130a.printSum();
-            ricoh_MP_2014AD.printSum();
+            epsonL366.printSum();
+            hpLaserJetProM130a.printSum();
+            ricohMP2014AD.printSum();
 
-            hp_LaserJet_Pro_M130a.setAmountOfPaper(150);
+            hpLaserJetProM130a.setAmountOfPaper(150);
 
             Printer.printStaticSum();
 
